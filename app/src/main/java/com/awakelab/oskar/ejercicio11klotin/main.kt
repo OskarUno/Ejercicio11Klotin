8b42b9a7aa2544c622d99ce60cf7c45ed760774f
OskarUno <oorellana80@gmail.com>
2023-07-17 01:47:36 -0400

    Ejercicio 11A, se agrega parte C

--- app/src/main/java/com/awakelab/oskar/ejercicio11klotin/main.kt
+++ app/src/main/java/com/awakelab/oskar/ejercicio11klotin/main.kt
@@ -1,5 +1,5 @@
 package com.awakelab.oskar.ejercicio11klotin
-
+/*
 fun main() {
     var n1: Int = 10
     val n2: Int = 33
@@ -12,4 +12,26 @@ fun main() {
     val promedio = suma / 3
     println("Promedio: $promedio")
 
+}
+*/
+
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        println("Like to: $hobby")
+        if(referrer == null){
+            println("Doesn't have a referrer")
+        }else{
+            println("Has a referrer named ${referrer.name}, Has a referrer named $hobby ")
+        }
+
+    }
 }
